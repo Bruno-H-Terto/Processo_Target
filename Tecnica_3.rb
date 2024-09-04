bbe342a41bf6933468bc1f714256b95b77d4c8f7
Bruno-H-Terto <herculanoterto@gmail.com>
2024-09-04 02:25:58 -0300

    Atualização da resposta do teste 3 com os dados json presentes na plataforma Gupy

--- Tecnica_3.rb
+++ Tecnica_3.rb
@@ -1,33 +1,49 @@
 require 'json'
 
-json_data = '{
-  "faturamento_diario_30_dias": [1000, 1500, 0, 1200, 700, 0, 0,
-                        2000, 1200, 1100, 1300, 1400, 0, 0,
-                        1600, 1500, 1400, 400, 1200, 0, 0,
-                        2300, 0, 1200, 3400, 720, 0, 0,
-                        780, 450, 1900, 840, 600, 0, 0,
-                        4100, 2100, 300, 700, 900, 0, 0,
-                        780, 200]
-}'
-
+json_data = '[
+  {"dia": 1, "valor": 22174.1664},
+  {"dia": 2, "valor": 24537.6698},
+  {"dia": 3, "valor": 26139.6134},
+  {"dia": 4, "valor": 0.0},
+  {"dia": 5, "valor": 0.0},
+  {"dia": 6, "valor": 26742.6612},
+  {"dia": 7, "valor": 0.0},
+  {"dia": 8, "valor": 42889.2258},
+  {"dia": 9, "valor": 46251.174},
+  {"dia": 10, "valor": 11191.4722},
+  {"dia": 11, "valor": 0.0},
+  {"dia": 12, "valor": 0.0},
+  {"dia": 13, "valor": 3847.4823},
+  {"dia": 14, "valor": 373.7838},
+  {"dia": 15, "valor": 2659.7563},
+  {"dia": 16, "valor": 48924.2448},
+  {"dia": 17, "valor": 18419.2614},
+  {"dia": 18, "valor": 0.0},
+  {"dia": 19, "valor": 0.0},
+  {"dia": 20, "valor": 35240.1826},
+  {"dia": 21, "valor": 43829.1667},
+  {"dia": 22, "valor": 18235.6852},
+  {"dia": 23, "valor": 4355.0662},
+  {"dia": 24, "valor": 13327.1025},
+  {"dia": 25, "valor": 0.0},
+  {"dia": 26, "valor": 0.0},
+  {"dia": 27, "valor": 25681.8318},
+  {"dia": 28, "valor": 1718.1221},
+  {"dia": 29, "valor": 13220.495},
+  {"dia": 30, "valor": 8414.61}
+]'
 
 dados = JSON.parse(json_data)
-faturamento_mes = dados['faturamento_diario_30_dias']
 
-
-faturamento_acima_de_zero = faturamento_mes.reject { |valor_dia| valor_dia == 0 }
-
+faturamento_acima_de_zero = dados.map { |dia| dia['valor'] }.reject { |valor| valor == 0 }
 
 menor_faturamento = faturamento_acima_de_zero.min
 maior_faturamento = faturamento_acima_de_zero.max
 
-
 media_faturamento = faturamento_acima_de_zero.sum.to_f / faturamento_acima_de_zero.size
 
+dias_acima_da_media = faturamento_acima_de_zero.count { |valor| valor > media_faturamento }
 
-dias_acima_da_media = faturamento_acima_de_zero.count { |faturamento_dia| faturamento_dia > media_faturamento }
-
-
-puts "Menor valor de faturamento: #{menor_faturamento}"
-puts "Maior valor de faturamento: #{maior_faturamento}"
+puts "Menor valor de faturamento: R$ #{'%.2f' % menor_faturamento}"
+puts "Maior valor de faturamento: R$ #{'%.2f' % maior_faturamento}"
 puts "Número de dias com faturamento acima da média: #{dias_acima_da_media}"
